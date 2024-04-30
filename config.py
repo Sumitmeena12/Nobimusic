5ed4df92a31fe76b60a0633101366faf23c38a23
Monster dragon <165640633+Sumitmeena12@users.noreply.github.com>
2024-04-30 23:25:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -47,7 +47,7 @@ GIT_TOKEN = getenv(
 )  # Fill this variable if your upstream repository is private
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/dragondpworld")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+2i2Fm7uVG-w4YTBl")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/apnihavelichattingxd")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", True))
